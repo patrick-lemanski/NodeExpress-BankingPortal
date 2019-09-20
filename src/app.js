40d67b8357badfe49380256b8443c373a1a3d722
patrick-lemanski <patrick.lemanski2@volkswagen.de>
2019-09-20 16:11:28 +0200

    module2

--- src/app.js
+++ src/app.js
@@ -10,9 +10,30 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), {encoding: 'utf-8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), {encoding: 'utf-8'});
+const users = JSON.parse(userData);
+
+
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'});
+    res.render('index', {title: 'Account Summary', accounts: accounts});
 });
+
+app.get('/savings', (req, res) => {
+    res.render('account', {title: 'Account Summary', account: accounts.savings});
+});
+app.get('/checking', (req, res) => {
+    res.render('account', {title: 'Account Summary', account: accounts.checking});
+});
+app.get('/credit', (req, res) => {
+    res.render('account', {title: 'Account Summary', account: accounts.credit});
+});
+app.get('/profile', (req, res) => {
+    res.render('profile', {title: 'Profile', user: users[0]});
+});
+
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
