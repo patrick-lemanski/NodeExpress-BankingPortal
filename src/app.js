edd26af532b40e8d8f670f222aceaae4e46917f8
patrick-lemanski <patrick.lemanski2@volkswagen.de>
2019-09-23 14:41:52 +0200

    module5

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 const express = require('express');
 const {accounts, users, writeJSON} = require('./data');
+
 const app = express();
+
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,41 +17,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary', accounts: accounts});
 });
 
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10) + parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: "Transfer Completed"});
-});
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit});
-});
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', {message: "Payment Successful", account: accounts.credit});
-});
 
 
-app.get('/savings', (req, res) => {
-    res.render('account', {title: 'Account Summary', account: accounts.savings});
-});
-app.get('/checking', (req, res) => {
-    res.render('account', {title: 'Account Summary', account: accounts.checking});
-});
-app.get('/credit', (req, res) => {
-    res.render('account', {title: 'Account Summary', account: accounts.credit});
-});
+
+
 app.get('/profile', (req, res) => {
     res.render('profile', {title: 'Profile', user: users[0]});
 });
