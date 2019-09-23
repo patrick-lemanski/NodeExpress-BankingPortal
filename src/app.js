15df57d2f5eb1b139633a370f5e809e48310787e
patrick-lemanski <patrick.lemanski2@volkswagen.de>
2019-09-23 13:44:46 +0200

    module3

--- src/app.js
+++ src/app.js
@@ -9,17 +9,40 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), {encoding: 'utf-8'});
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), {encoding: 'utf8'});
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), {encoding: 'utf-8'});
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), {encoding: 'utf8'});
 const users = JSON.parse(userData);
 
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary', accounts: accounts});
 });
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer');
+});
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: "Transfer Completed"});
+});
+app.get('/payment', (req, res) => {
+    res.render('payment', {account: accounts.credit});
+});
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+});
+
 
 app.get('/savings', (req, res) => {
     res.render('account', {title: 'Account Summary', account: accounts.savings});
